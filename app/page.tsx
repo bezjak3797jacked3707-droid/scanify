e2e616675fe2cecc39f067a12db25deda2f1ca51
Emil Gisslen Bezjak <bezjak3797jacked3707@gmail.com>
2026-03-31 21:38:43 +0200

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
-export default function Home() {
+import { supabase } from '@/lib/supabase'
+
+export default async function Home() {
+  const { data, error } = await supabase
+    .from('test')
+    .select('*')
+
   return (
     <main style={{ padding: "40px" }}>
       <h1>Scanify</h1>
-      <p>My first real website 🚀</p>
+      <pre>{JSON.stringify({ data, error }, null, 2)}</pre>
     </main>
-  );
+  )
 }
